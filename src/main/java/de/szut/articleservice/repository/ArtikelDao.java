c548f8f06e1bce2ea743997fb71cc7ecfd6a5913
ShadowTryDragon <90843434+ShadowTryDragon@users.noreply.github.com>
2025-02-19 10:01:54 +0100

    Update ArtikelDao.java

--- src/main/java/de/szut/articleservice/repository/ArtikelDao.java
+++ src/main/java/de/szut/articleservice/repository/ArtikelDao.java
@@ -2,6 +2,8 @@ package de.szut.articleservice.repository;
 
 import de.szut.articleservice.model.Artikel;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -9,21 +11,21 @@ public class ArtikelDao {
     Map<Long, Artikel> artikelListe;
 
     public ArtikelDao() {
-
+        initHashMap();
     }
     public void initHashMap() {
-
+        artikelListe = new HashMap<>();
+        insert(new Artikel(1,"Marques", "Ein Spiezelles Objekt",80.99,99.99));
     }
     public void insert(Artikel artikel) {
         artikelListe.put(artikel.getId(),artikel);
-
     }
-    public Artikel findbyId(long id) {
+    public Artikel findById(long id) {
         return artikelListe.get(id);
     }
 
     public List<Artikel> findAll() {
-        return artikelListe.values().stream().toList();
+        return new ArrayList<Artikel>(artikelListe.values());
     }
 
     public void update(Artikel artikel) {
