d9f4e9595d2bb126a4b6a6d8bca6811f1193c99c
ShadowTryDragon <90843434+ShadowTryDragon@users.noreply.github.com>
2025-02-19 09:19:04 +0100

    Update ArtikelController.java

--- src/main/java/de/szut/articleservice/controller/ArtikelController.java
+++ src/main/java/de/szut/articleservice/controller/ArtikelController.java
@@ -4,13 +4,12 @@ import de.szut.articleservice.model.Artikel;
 import de.szut.articleservice.service.ArtikelService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/myappdata/articles")
+@RequestMapping
 public class ArtikelController {
 
     private ArtikelService artikelService;
@@ -19,9 +18,28 @@ public class ArtikelController {
         artikelService = new ArtikelService();
     }
 
-    public ResponseEntity<List<Artikel>> getArtikel() {
-        List<Artikel> artikelList = artikelService.read();
+    @GetMapping("/{id}")
+    public ResponseEntity<List<Artikel>> getArtikel(@PathVariable long id) {
+        List<Artikel> artikelList = artikelService.readAll();
         return  new ResponseEntity<>(artikelList, HttpStatus.OK);
+
+    }
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Artikel> delete(@PathVariable long id) {
+  artikelService.delete(id);
+        return  new ResponseEntity(HttpStatus.OK);
+
+    }
+    @PostMapping
+    public ResponseEntity<Artikel> add(@RequestBody Artikel request) {
+        Artikel artikel = artikelService.add(request);
+        return  new ResponseEntity<>(artikel, HttpStatus.OK);
+
+    }
+    @PutMapping
+    public ResponseEntity<?> update(@RequestBody Artikel request) {
+        artikelService.update(request);
+        return  new ResponseEntity(HttpStatus.OK);
 
     }
 
